4f0dc49bec53811c0335e5aafd1c28e93df61560
Carson <c_halter@live.ca>
2022-09-07 07:00:23 -0600

    attempt to create winning parameters

--- javaScript/script.js
+++ javaScript/script.js
@@ -18,82 +18,165 @@ const gameBoard = (() => {
       gameCell.addEventListener("click", function () {
         const mark = document.createElement("h1");
         mark.setAttribute("class", "game-board-mark");
+        mark.setAttribute("value", currentPlayer.marker);
         mark.textContent = currentPlayer.marker;
         gameCell.appendChild(mark);
         console.log(`cell${i}${j}`);
 
         // Winning conditions
-        if (`cell${i + 1}${j}` != null) {
-          let cellDown1 = document.getElementById(`cell${i + 1}${j}`);
-          console.log("cellDown1", cellDown1);
-        }
-        if (`cell${i - 1}${j}` != null) {
-          let cellUp1 = document.getElementById(`cell${i - 1}${j}`);
-          console.log("cellUp1", cellUp1);
-        }
-        if (`cell${i}${j - 1}` != null) {
-          let cellLeft1 = document.getElementById(`cell${i}${j - 1}`);
-          console.log("cellLeft1", cellLeft1);
-        }
-        if (`cell${i}${j + 1}` != null) {
-          let cellRight1 = document.getElementById(`cell${i}${j + 1}`);
-          console.log("cellRight1", cellRight1);
-        }
-        if (`cell${i - 1}${j - 1}` != null) {
-          let cellUp1Left1 = document.getElementById(`cell${i - 1}${j - 1}`);
-          console.log("cellUp1Left1", cellUp1Left1);
-        }
-        if (`cell${i - 1}${j + 1}` != null) {
-          let cellUp1Right1 = document.getElementById(`cell${i - 1}${j + 1}`);
-          console.log("cellUp1Right1", cellUp1Right1);
-        }
-        if (`cell${i + 1}${j - 1}` != null) {
-          let cellDown1Left1 = document.getElementById(`cell${i + 1}${j - 1}`);
-          console.log("cellDown1Left1", cellDown1Left1);
-        }
-        if (`cell${i + 1}${j + 1}` != null) {
-          let cellDown1Right1 = document.getElementById(`cell${i + 1}${j + 1}`);
-          console.log("cellDown1Right1", cellDown1Right1);
-        }
-        if (`cell${i - 2}${j}` != null) {
-          let cellUp2 = document.getElementById(`cell${i - 2}${j}`);
-          console.log("cellUp2", cellUp2);
-        }
-        if (`cell${i + 2}${j}` != null) {
-          let cellDown2 = document.getElementById(`cell${i + 2}${j}`);
-          console.log("cellDown2", cellDown2);
-        }
-        if (`cell${i}${j + 2}` != null) {
-          let cellRight2 = document.getElementById(`cell${i}${j + 2}`);
-          console.log("cellRight2", cellRight2);
-        }
-        if (`cell${i}${j - 2}` != null) {
-          let cellLeft2 = document.getElementById(`cell${i}${j - 2}`);
-          console.log("cellLeft2", cellLeft2);
-        }
-        if (`cell${i + 2}${j - 2}` != null) {
-          let cellDown2Left2 = document.getElementById(`cell${i + 2}${j - 2}`);
-          console.log("cellDown2Left2", cellDown2Left2);
-        }
-        if (`cell${i + 2}${j + 2}` != null) {
-          let cellDown2Right2 = document.getElementById(`cell${i + 2}${j + 2}`);
-          console.log("cellDown2Right2", cellDown2Right2);
-        }
-        if (`cell${i - 2}${j - 2}` != null) {
-          let cellUp2Left2 = document.getElementById(`cell${i - 2}${j - 2}`);
-          console.log("cellUp2Left2", cellUp2Left2);
-        }
-        if (`cell${i - 2}${j + 2}` != null) {
-          let cellUp2Right2 = document.getElementById(`cell${i - 2}${j + 2}`);
-          console.log("cellUp2Right2", cellUp2Right2);
+
+        let cellUp1 = document.getElementById(`cell${i - 1}${j}`);
+        let cellDown1 = document.getElementById(`cell${i + 1}${j}`);
+        let cellLeft1 = document.getElementById(`cell${i}${j - 1}`);
+        let cellRight1 = document.getElementById(`cell${i}${j + 1}`);
+        let cellUp1Left1 = document.getElementById(`cell${i - 1}${j - 1}`);
+        let cellUp1Right1 = document.getElementById(`cell${i - 1}${j + 1}`);
+        let cellDown1Left1 = document.getElementById(`cell${i + 1}${j - 1}`);
+        let cellDown1Right1 = document.getElementById(`cell${i + 1}${j + 1}`);
+        let cellUp2 = document.getElementById(`cell${i - 2}${j}`);
+        let cellDown2 = document.getElementById(`cell${i + 2}${j}`);
+        let cellRight2 = document.getElementById(`cell${i}${j + 2}`);
+        let cellLeft2 = document.getElementById(`cell${i}${j - 2}`);
+        let cellDown2Left2 = document.getElementById(`cell${i + 2}${j - 2}`);
+        let cellDown2Right2 = document.getElementById(`cell${i + 2}${j + 2}`);
+        let cellUp2Left2 = document.getElementById(`cell${i - 2}${j - 2}`);
+        let cellUp2Right2 = document.getElementById(`cell${i - 2}${j + 2}`);
+
+        if (cellUp1 !== null && this.textContent === cellUp1.textContent) {
+          if (
+            cellDown1 !== null &&
+            this.textContent === cellDown1.textContent
+          ) {
+            if (
+              this.textContent === cellUp1.textContent &&
+              this.textContent === cellDown1.textContent
+            ) {
+              alert(`${currentPlayer.name} wins!!`);
+              console.log(`${currentPlayer.name} wins!!`);
+              console.log(
+                "-----------------------------------------------------"
+              );
+            } else {
+              console.log(cellUp1.value);
+              console.log(cellDown1.value);
+              console.log("test1");
+
+              switchPlayers();
+            }
+          } else if (!cellDown1) {
+            {
+              console.log("test2");
+
+              console.log(this);
+              switchPlayers();
+            }
+          }
+        } else if (
+          cellLeft1 !== null &&
+          this.textContent === cellLeft1.textContent
+        ) {
+          if (
+            cellRight1 !== null &&
+            this.textContent === cellRight1.textContent
+          ) {
+            if (
+              this.textContent === cellLeft1.textContent &&
+              this.textContent === cellRight1.textContent
+            ) {
+              alert(`${currentPlayer.name} wins!!`);
+              console.log(`${currentPlayer.name} wins!!`);
+              console.log(
+                "-----------------------------------------------------"
+              );
+            } else {
+              console.log("test3");
+
+              switchPlayers();
+            }
+          } else if (!cellRight1) {
+            {
+              console.log("test4");
+
+              console.log(this);
+              switchPlayers();
+            }
+          }
+        } else {
+          console.log("test5");
+
+          switchPlayers();
         }
 
-        if (currentPlayer.marker === "X") {
-          // Switch turns
-          currentPlayer = playerO;
-        } else if (currentPlayer.marker === "O") {
-          currentPlayer = playerX;
+        // if (`cell${i}${j - 1}` != null) {
+        //   let cellLeft1 = document.getElementById(`cell${i}${j - 1}`);
+        //   console.log("cellLeft1", cellLeft1);
+        // }
+        // if (`cell${i}${j + 1}` != null) {
+        //   let cellRight1 = document.getElementById(`cell${i}${j + 1}`);
+        //   console.log("cellRight1", cellRight1);
+        // }
+        // if (`cell${i - 1}${j - 1}` != null) {
+        //   let cellUp1Left1 = document.getElementById(`cell${i - 1}${j - 1}`);
+        //   console.log("cellUp1Left1", cellUp1Left1);
+        // }
+        // if (`cell${i - 1}${j + 1}` != null) {
+        //   let cellUp1Right1 = document.getElementById(`cell${i - 1}${j + 1}`);
+        //   console.log("cellUp1Right1", cellUp1Right1);
+        // }
+        // if (`cell${i + 1}${j - 1}` != null) {
+        //   let cellDown1Left1 = document.getElementById(`cell${i + 1}${j - 1}`);
+        //   console.log("cellDown1Left1", cellDown1Left1);
+        // }
+        // if (`cell${i + 1}${j + 1}` != null) {
+        //   let cellDown1Right1 = document.getElementById(`cell${i + 1}${j + 1}`);
+        //   console.log("cellDown1Right1", cellDown1Right1);
+        // }
+        // if (`cell${i - 2}${j}` != null) {
+        //   let cellUp2 = document.getElementById(`cell${i - 2}${j}`);
+        //   console.log("cellUp2", cellUp2);
+        // }
+        // if (`cell${i + 2}${j}` != null) {
+        //   let cellDown2 = document.getElementById(`cell${i + 2}${j}`);
+        //   console.log("cellDown2", cellDown2);
+        // }
+        // if (`cell${i}${j + 2}` != null) {
+        //   let cellRight2 = document.getElementById(`cell${i}${j + 2}`);
+        //   console.log("cellRight2", cellRight2);
+        // }
+        // if (`cell${i}${j - 2}` != null) {
+        //   let cellLeft2 = document.getElementById(`cell${i}${j - 2}`);
+        //   console.log("cellLeft2", cellLeft2);
+        // }
+        // if (`cell${i + 2}${j - 2}` != null) {
+        //   let cellDown2Left2 = document.getElementById(`cell${i + 2}${j - 2}`);
+        //   console.log("cellDown2Left2", cellDown2Left2);
+        // }
+        // if (`cell${i + 2}${j + 2}` != null) {
+        //   let cellDown2Right2 = document.getElementById(`cell${i + 2}${j + 2}`);
+        //   console.log("cellDown2Right2", cellDown2Right2);
+        // }
+        // if (`cell${i - 2}${j - 2}` != null) {
+        //   let cellUp2Left2 = document.getElementById(`cell${i - 2}${j - 2}`);
+        //   console.log("cellUp2Left2", cellUp2Left2);
+        // }
+        // if (`cell${i - 2}${j + 2}` != null) {
+        //   let cellUp2Right2 = document.getElementById(`cell${i - 2}${j + 2}`);
+        //   console.log("cellUp2Right2", cellUp2Right2);
+        // }
+
+        // if (gameCell.textContent == cellDown1.textContent) {
+        //   console.log(`${currentPlayer} wins!!`);
+        // }
+
+        // Switch turns
+        function switchPlayers() {
+          if (currentPlayer.marker === "X") {
+            currentPlayer = playerO;
+          } else if (currentPlayer.marker === "O") {
+            currentPlayer = playerX;
+          }
         }
+        //switchPlayers();
       });
 
       gameboardArray.push(gameCell);
@@ -220,6 +303,7 @@ const displayController = (() => {
     if (x < 3) return;
     popupOverlay.classList.remove("active");
     console.log("bang2");
+    console.log("-----------------------------------------------------");
 
     popupOverlay.classList.remove("active");
   }
